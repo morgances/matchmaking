fbea4a5e42e18d8962267356e0b32cca33c43184
wanghuajian620 <154659041@qq.com>
2018-09-21 19:19:08 +0800

    write lovecircle page

---
+++ marriage/src/libs/manual-content.js
@@ -0,0 +1,16 @@
+/**
+ * Revision History:
+ *   Initial: 2018/09/21          Wang Huajian
+ */
+
+export const LoveCircle = [
+  {
+    avatar: 'https://images.pexels.com/photos/373945/pexels-photo-373945.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    name: '我想静静',
+    vip: '../assets/images/v.png',
+    age: '28岁 · 北京 · 165cm ·双子座',
+    date: '15:55',
+    content: '我更新了个人相册，快来看看吧! 又到了一年的中秋佳节，只可惜自己独身异乡',
+    picture: 'https://images.pexels.com/photos/713132/pexels-photo-713132.jpeg?auto=compress&cs=tinysrgb&h=350'
+  }
+]
