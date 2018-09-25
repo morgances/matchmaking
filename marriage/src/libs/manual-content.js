662bf0ca3eddcd1ea9f0f02d59e94047addc6c04
wanghuajian620 <154659041@qq.com>
2018-09-26 00:30:50 +0800

    write mall page

--- marriage/src/libs/manual-content.js
+++ marriage/src/libs/manual-content.js
@@ -12,14 +12,6 @@ export const LoveCircle = [
     date: '15:55',
     content: '我更新了个人相册，快来看看吧! 又到了一年的中秋佳节，只可惜自己独身异乡',
     picture: 'https://images.pexels.com/photos/713132/pexels-photo-713132.jpeg?auto=compress&cs=tinysrgb&h=350'
-  }, {
-    avatar: 'https://images.pexels.com/photos/373945/pexels-photo-373945.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    name: '我想静静',
-    vip: '../assets/images/v.png',
-    age: '28岁 · 北京 · 165cm ·双子座',
-    date: '15:55',
-    content: '我更新了个人相册，快来看看吧! 又到了一年的中秋佳节，只可惜自己独身异乡',
-    picture: 'https://images.pexels.com/photos/1226458/pexels-photo-1226458.jpeg?auto=compress&cs=tinysrgb&h=350'
   }
 ]
 
@@ -45,4 +37,9 @@ export const SwiperPicture = [
   'https://images.pexels.com/photos/769584/pexels-photo-769584.jpeg?auto=compress&cs=tinysrgb&h=350',
   'https://images.pexels.com/photos/1079796/pexels-photo-1079796.jpeg?auto=compress&cs=tinysrgb&h=350',
   'https://images.pexels.com/photos/1386603/pexels-photo-1386603.jpeg?auto=compress&cs=tinysrgb&h=350'
+]
+
+export const SwiperPhoto = [
+  'https://img.kemanyun.com/kemanyun/7444/2018-09-25/5baa10a5d0f93.png',
+  'https://img.kemanyun.com/kemanyun/7444/2018-09-25/5baa10af07ef1.png '
 ]
