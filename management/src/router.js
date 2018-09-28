bb4ddfdc379c6f97c377138e27bbdc10cc1d66b8
liuqiiiiiii <32216634+liuqiiiiiii@users.noreply.github.com>
2018-09-28 17:42:27 +0800

    initiating user login page

--- management/src/router.js
+++ management/src/router.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
 import Products from './routes/Products';
+import UserLayout from './routes/UserLayout';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/" exact component={IndexPage} />
+        <Route path="/IndexPage" exact component={IndexPage} />
+
         <Route path="/products" exact component={Products} />
+
+        <Route path="/" exact component={UserLayout} />
       </Switch>
     </Router>
   );
