e6ed58b817368f87d8646cf09af83436e3d157dd
liuqiiiiiii <982252163@qq.com>
2018-10-23 23:45:56 +0800

    Basiclly complete the profile page

--- management/src/pages/Profile/BasicProfile.js
+++ management/src/pages/Profile/BasicProfile.js
@@ -1,27 +1,49 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
 import { Card, Badge, Table, Divider } from 'antd';
-import DescriptionList from '@/components/DescriptionList';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 import styles from './BasicProfile.less';
 
-const { Description } = DescriptionList;
-
 const progressColumns = [
   {
-    title: '时间',
-    dataIndex: 'time',
-    key: 'time',
+    title: '订单ID',
+    dataIndex: 'id',
+    key: 'id',
   },
   {
-    title: '当前进度',
-    dataIndex: 'rate',
-    key: 'rate',
+    title: '用户ID',
+    dataIndex: 'open_id',
+    key: 'open_id',
   },
   {
-    title: '状态',
-    dataIndex: 'status',
-    key: 'status',
+    title: '商品ID',
+    dataIndex: 'goods_id',
+    key: 'goods_id',
+  },
+  {
+    title: '买家姓名',
+    dataIndex: 'buyer_name',
+    key: 'buyer_name',
+  },
+  {
+    title: '商品名称',
+    dataIndex: 'goods_name',
+    key: 'goods_name',
+  },
+  {
+    title: '兑换时间',
+    dataIndex: 'date_time',
+    key: 'date_time',
+  },
+  {
+    title: '使用积分',
+    dataIndex: 'cost',
+    key: 'cost',
+  },
+  {
+    title: '订单状态',
+    dataIndex: 'finished',
+    key: 'finished',
     render: text =>
       text === 'success' ? (
         <Badge status="success" text="成功" />
@@ -29,22 +51,13 @@ const progressColumns = [
         <Badge status="processing" text="进行中" />
       ),
   },
-  {
-    title: '操作员ID',
-    dataIndex: 'operator',
-    key: 'operator',
-  },
-  {
-    title: '耗时',
-    dataIndex: 'cost',
-    key: 'cost',
-  },
 ];
 
 @connect(({ profile, loading }) => ({
   profile,
   loading: loading.effects['profile/fetchBasic'],
 }))
+
 class BasicProfile extends Component {
   componentDidMount() {
     const { dispatch } = this.props;
@@ -70,6 +83,7 @@ class BasicProfile extends Component {
         amount,
       });
     }
+
     const renderContent = (value, row, index) => {
       const obj = {
         children: value,
@@ -80,95 +94,12 @@ class BasicProfile extends Component {
       }
       return obj;
     };
-    const goodsColumns = [
-      {
-        title: '商品编号',
-        dataIndex: 'id',
-        key: 'id',
-        render: (text, row, index) => {
-          if (index < basicGoods.length) {
-            return <a href="">{text}</a>;
-          }
-          return {
-            children: <span style={{ fontWeight: 600 }}>总计</span>,
-            props: {
-              colSpan: 4,
-            },
-          };
-        },
-      },
-      {
-        title: '商品名称',
-        dataIndex: 'name',
-        key: 'name',
-        render: renderContent,
-      },
-      {
-        title: '商品条码',
-        dataIndex: 'barcode',
-        key: 'barcode',
-        render: renderContent,
-      },
-      {
-        title: '单价',
-        dataIndex: 'price',
-        key: 'price',
-        align: 'right',
-        render: renderContent,
-      },
-      {
-        title: '数量（件）',
-        dataIndex: 'num',
-        key: 'num',
-        align: 'right',
-        render: (text, row, index) => {
-          if (index < basicGoods.length) {
-            return text;
-          }
-          return <span style={{ fontWeight: 600 }}>{text}</span>;
-        },
-      },
-      {
-        title: '金额',
-        dataIndex: 'amount',
-        key: 'amount',
-        align: 'right',
-        render: (text, row, index) => {
-          if (index < basicGoods.length) {
-            return text;
-          }
-          return <span style={{ fontWeight: 600 }}>{text}</span>;
-        },
-      },
-    ];
+
     return (
-      <PageHeaderWrapper title="基础详情页">
+      <PageHeaderWrapper>
         <Card bordered={false}>
-          <DescriptionList size="large" title="退款申请" style={{ marginBottom: 32 }}>
-            <Description term="取货单号">1000000000</Description>
-            <Description term="状态">已取货</Description>
-            <Description term="销售单号">1234123421</Description>
-            <Description term="子订单">3214321432</Description>
-          </DescriptionList>
           <Divider style={{ marginBottom: 32 }} />
-          <DescriptionList size="large" title="用户信息" style={{ marginBottom: 32 }}>
-            <Description term="用户姓名">付小小</Description>
-            <Description term="联系电话">18100000000</Description>
-            <Description term="常用快递">菜鸟仓储</Description>
-            <Description term="取货地址">浙江省杭州市西湖区万塘路18号</Description>
-            <Description term="备注">无</Description>
-          </DescriptionList>
-          <Divider style={{ marginBottom: 32 }} />
-          <div className={styles.title}>退货商品</div>
-          <Table
-            style={{ marginBottom: 24 }}
-            pagination={false}
-            loading={loading}
-            dataSource={goodsData}
-            columns={goodsColumns}
-            rowKey="id"
-          />
-          <div className={styles.title}>退货进度</div>
+          <div className={styles.title}>积分交易处理</div>
           <Table
             style={{ marginBottom: 16 }}
             pagination={false}
