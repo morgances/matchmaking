d165fac82dbf10aa28cdd3616a6dc7fa3f2d3c7b
liuqiiiiiii <982252163@qq.com>
2018-11-01 16:27:28 +0800

    improve information page's simplycity

--- management/src/models/information.js
+++ management/src/models/information.js
@@ -10,23 +10,28 @@ export default {
   effects: {
     *fetchInformation({ payload }, { call, put }) {
       const response = yield call(submitInformation, payload);
-      console.log('后台返回的值' ,response)
-      if (payload.status === 0) {
+      console.log(response,'aaaaaaaaaaa')
+      if (response.status === 0) {
         yield put({
           type: 'changeInformation',
           payload: response.data,
         });
+      } else {
+        yield put ({
+          type: 'changeInformation',
+          payload: 'nothing',
+        })
       }
       return response.data
     },
+  },
 
-    reducers: {
-      changeInformation(state, { payload }) {
-        return {
-          ...state,
-          information: payload
-        };
-      },
+  reducers: {
+    changeInformation(state, { payload }) {
+      return {
+        ...state,
+        information: payload
+      };
     },
-  }
+  },
 };
